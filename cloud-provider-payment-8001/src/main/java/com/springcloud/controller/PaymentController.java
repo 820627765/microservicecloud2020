0d1bea240cb3c7f3a0446d2816635eeb1f97f56d
820627765 <820627765@qq.com>
2021-07-04 19:55:38 +0800

    'update'

--- cloud-provider-payment-8001/src/main/java/com/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/springcloud/controller/PaymentController.java
@@ -1,18 +1,26 @@
 package com.springcloud.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.DiscoveryClient;
 import com.springcloud.entity.CommonResult;
 import com.springcloud.entity.Payment;
 import com.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -33,5 +41,18 @@ public class PaymentController {
         } else {
             return new CommonResult<Payment>(444, "没有对应记录,查询ID: " + id, null);
         }
+    }
+
+    @GetMapping(value="/payment/discovery")
+    public Object discovery(){
+        //根据微服务名称获取 注册到eureka上的这个服务名对应的所有实例信息
+        List<InstanceInfo> instances = discoveryClient.getInstancesById("CLOUD-PAYMENT-SERVICE");
+        for(InstanceInfo info : instances){
+            //获取实例对应的ip 和 端口号
+            System.out.println(info.getIPAddr() + ":" + info.getPort());
+            System.out.println(info.getHomePageUrl());
+        }
+
+        return discoveryClient;
     }
 }
